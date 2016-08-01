68417df9bf69c4bd39738f240d59e152a247785e
Emanuele Libralato <e.libralato@gmail.com>
2016-08-01 18:27:02 +0200

    Removed unnecessary spaces

--- js/app/routing.js
+++ js/app/routing.js
@@ -3,18 +3,16 @@ angular
   .config(config);
 
 function config($stateProvider, $locationProvider) {
-
-    $locationProvider.html5Mode(true);
-
-    $stateProvider
-      .state('home', {
-        url: "/",
-        templateUrl: "js/app/modules/base/home.html"
-      })
-      .state('mod', {
-        url: "/mod",
-        templateUrl: "js/app/modules/module/module.html",
-        controller: 'ModuleController',
-        controllerAs: 'vm'
-      });
+  $locationProvider.html5Mode(true);
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "js/app/modules/base/home.html"
+    })
+    .state('mod', {
+      url: "/mod",
+      templateUrl: "js/app/modules/module/module.html",
+      controller: 'ModuleController',
+      controllerAs: 'vm'
+    });
 }
